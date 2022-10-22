669de3d8df4327fbff9ebf4ef524a5423b321ac5
AvryasovIlya <115112160+AvryasovIlya@users.noreply.github.com>
2022-10-22 16:21:03 +0300

    Переделка Лаб 3

---
+++ Переделка Л.Р 3/task_1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+# TODO расписать упрощение выражения
+
+result = True and True or False and False  #- отриц
+result = True or False # -и
+result = True
+
+print(result)
